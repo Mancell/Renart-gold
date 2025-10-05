39de0b418bc7cb8ce28ed1fd3579a70ff502d8ca
Mancell <mancell@users.noreply.github.com>
2025-10-05 16:36:44 +0300

    chore: config cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => ({
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:8082",
+        target: "https://renart-backend-api.vercel.app",
         changeOrigin: true,
       },
     },
